9a0b936241a5d6903081b77ee34140be137206c4
Pravin Raju <tmpravinraju@gmail.com>
2024-11-12 21:56:29 +0530

    updated habit.js

--- routes/habits.js
+++ routes/habits.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Habit = require('../models/habit');
+const Habit = require('../models/Habit');
 
 // Display habits
 router.get('/', async (req, res) => {
